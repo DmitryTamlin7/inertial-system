c0251e8f7cbed56cf3c4e43fb9434b1221d29e4e
admin <admin@admins-MacBook-Air.local>
2025-05-23 13:24:57 +0300

    add main

--- main/run_simulation.py
+++ main/run_simulation.py
@@ -1,10 +1,40 @@
-def run_simulation(duration=10.0):
-    # 1. Создаёт экземпляры MEMSSimulator и KalmanFilter
-    # 2. Генерирует «истинное» движение (угол и скорость)
-    # 3. В цикле по времени:
-    #       - Получает данные с эмулятора (gyro и accel)
-    #       - Передаёт их в фильтр Калмана (predict + update)
-    #        - Сохраняет: t, true_angle, gyro_meas, accel_meas, filtered_angle
-    # 4. Сохраняет всё в CSV или возвращает списки
+import numpy as np
+import csv
+
+from emulator.mems_simulator import MEMSSimulator
+from kalman_filter.kalman import KalmanFilter
+
+def run_simulation(duration=10.0, dt=0.01, save_to_csv=True, filename='simulation_output.csv'):
+    sim = MEMSSimulator(dt=dt)
+    kf = KalmanFilter(dt=dt)
 
 
+    time_steps = int(duration / dt)
+    true_angle = 0.0
+    true_rate = 1.0
+
+
+    data = []
+
+    for step in range(time_steps):
+        t = step * dt
+        true_angle += true_rate * dt
+
+        gyro_meas = sim.get_gyro(true_rate)
+        accel_meas = sim.get_accel(true_angle)
+
+        kf.predict(gyro_meas)
+        kf.update(accel_meas)
+
+        filtered_angle = kf.get_angle()
+
+        data.append((t, true_angle, gyro_meas, accel_meas, filtered_angle))
+
+
+    if save_to_csv:
+        with open(filename, mode='w', newline='') as file:
+            writer = csv.writer(file)
+            writer.writerow(['time', 'true_angle', 'gyro_meas', 'accel_meas', 'filtered_angle'])
+            writer.writerows(data)
+
+    return data
